d357e7ca1aaef0f49934a3a81b0db5b4f0ff8138
udit01 <uditjainiitd@gmail.com>
2018-03-16 14:57:37 +0530

    Some problem while using iterator, also, there are 2 methods to draw wireframes, important to convert between 2D and 3D otherwise...

--- glwidget.cpp
+++ glwidget.cpp
@@ -231,38 +231,39 @@ void Glwidget::draw()
 
 
 
-    glBegin(GL_QUADS);
-        glNormal3f(0,0,-1);
-        glVertex3f(-1,-1,0);
-        glVertex3f(-1,1,0);
-        glVertex3f(1,1,0);
-        glVertex3f(1,-1,0);
+//    glBegin(GL_QUADS);
+//        glNormal3f(0,0,-1);
+//        glVertex3f(-1,-1,0);
+//        glVertex3f(-1,1,0);
+//        glVertex3f(1,1,0);
+//        glVertex3f(1,-1,0);
 
-    glEnd();
+//    glEnd();
 
-    glBegin(GL_TRIANGLES);
-        glNormal3f(0,-1,0.707);
-        glVertex3f(-1,-1,0);
-        glVertex3f(1,-1,0);
-        glVertex3f(0,0,1.2);
-    glEnd();
-    glBegin(GL_TRIANGLES);
-        glNormal3f(1,0, 0.707);
-        glVertex3f(1,-1,0);
-        glVertex3f(1,1,0);
-        glVertex3f(0,0,1.2);
-    glEnd();
-    glBegin(GL_TRIANGLES);
-        glNormal3f(0,1,0.707);
-        glVertex3f(1,1,0);
-        glVertex3f(-1,1,0);
-        glVertex3f(0,0,1.2);
-    glEnd();
-    glBegin(GL_TRIANGLES);
-        glNormal3f(-1,0,0.707);
-        glVertex3f(-1,1,0);
-        glVertex3f(-1,-1,0);
-        glVertex3f(0,0,1.2);
-    glEnd();
+//    glBegin(GL_TRIANGLES);
+//        glNormal3f(0,-1,0.707);
+//        glVertex3f(-1,-1,0);
+//        glVertex3f(1,-1,0);
+//        glVertex3f(0,0,1.2);
+//    glEnd();
+//    glBegin(GL_TRIANGLES);
+//        glNormal3f(1,0, 0.707);
+//        glVertex3f(1,-1,0);
+//        glVertex3f(1,1,0);
+//        glVertex3f(0,0,1.2);
+//    glEnd();
+//    glBegin(GL_TRIANGLES);
+//        glNormal3f(0,1,0.707);
+//        glVertex3f(1,1,0);
+//        glVertex3f(-1,1,0);
+//        glVertex3f(0,0,1.2);
+//    glEnd();
+//    glBegin(GL_TRIANGLES);
+//        glNormal3f(-1,0,0.707);
+//        glVertex3f(-1,1,0);
+//        glVertex3f(-1,-1,0);
+//        glVertex3f(0,0,1.2);
+//    glEnd();
+
 }
 
